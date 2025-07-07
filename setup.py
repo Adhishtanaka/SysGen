6b4050f83719c5bef1c5ddf81a22d8fbfd3a5d2b
Adhishtanaka Kulasooriya <kulasoooriyaa@gmail.com>
2025-07-07 12:53:44 +0530

    fixed a bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sysgen",  
-    version="0.2.0",
+    version="0.2.1",
     author="Adhishtanaka",
     author_email="kulasoooriyaa@gmail.com",
     description="SysGen - High-quality synthetic datasets creating tool using Gemini API",
@@ -19,7 +19,8 @@ setup(
         "torch>=2.0.0",
         "transformers>=4.30.0",
         "numpy>=1.21.0",
-        "scipy>=1.7.0"
+        "scipy>=1.7.0",
+        "tf-keras"
     ],
     entry_points={
         "console_scripts": [
